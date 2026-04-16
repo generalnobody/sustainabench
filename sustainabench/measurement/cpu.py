c6c7509dbfb078fe3695d87fc0f9abd77d067ed7
generalnobody <ingmar.biemond@gmail.com>
2026-04-16 15:34:57 +0200

    Reworked cpu measurement to focus on what is important, reworked nvidia measurement for better  accuracy and more info

--- sustainabench/measurement/cpu.py
+++ sustainabench/measurement/cpu.py
@@ -4,25 +4,43 @@ from sustainabench.measurement.base import Measurement, register_measurement
 
 @register_measurement
 class CPUMeasurement(Measurement):
-
-    name = "cpu"
+    name = "cpu-usage"
     poll_interval = 0.1
     scope = "node"
 
     def start(self):
         self.samples = []
+        psutil.cpu_percent(interval=None) # Ignore first bogus value
+        psutil.cpu_percent(interval=None, percpu=True)
 
     def sample(self):
-        self.samples.append(psutil.cpu_percent())
+        self.samples.append((
+            psutil.cpu_percent(interval=None),
+            psutil.cpu_percent(interval=None, percpu=True)
+        ))
 
     def stop(self):
         pass
 
     def result(self):
         if not self.samples:
-            return {"cpu_avg": 0, "cpu_max": 0}
+            return {"cpu_avg": 0, "cpu_max": 0, "per-core": {}}
+
+        cpu_avg = sum(sample[0] for sample in self.samples) / len(self.samples)
+        cpu_max = sum(sample[0] for sample in self.samples)
+
+        per_cpu_avg = [
+            sum(col) / len(col)
+            for col in zip(*(sample[1] for sample in self.samples))
+        ]
+        per_cpu_max = [
+            max(col)
+            for col in zip(*(sample[1] for sample in self.samples))
+        ]
 
         return {
-            "cpu_avg": sum(self.samples) / len(self.samples),
-            "cpu_max": max(self.samples),
+            "cpu_avg": cpu_avg,
+            "cpu_max": cpu_max,
+            "per_cpu_avg": per_cpu_avg,
+            "per_cpu_max": per_cpu_max
         }
--- sustainabench/measurement/nvidia_smi.py
+++ sustainabench/measurement/nvidia_smi.py
@@ -1,30 +1,59 @@
 import pynvml
+import time
 from sustainabench.measurement.base import Measurement, register_measurement
 
 
 @register_measurement
-class CPUMeasurement(Measurement):
+class NvidiaGPUMeasurement(Measurement):
 
-    name = "nv-pow"
+    name = "gpu-nv"
     poll_interval = 0.1
     scope = "node"
 
     def start(self):
         self.samples = []
+        self.utils = []
         pynvml.nvmlInit()
         self.handle = pynvml.nvmlDeviceGetHandleByIndex(0)
 
     def sample(self):
-        self.samples.append(pynvml.nvmlDeviceGetPowerUsage(self.handle))
+        self.samples.append((pynvml.nvmlDeviceGetPowerUsage(self.handle), time.perf_counter())) # Create list of tuples (mW, timestamp)
+        self.utils.append(pynvml.nvmlDeviceGetUtilizationRates(self.handle).gpu)
 
     def stop(self):
         pynvml.nvmlShutdown()
-        pass
 
     def result(self):
-        if not self.samples:
-            return {"cpu_avg": 0, "cpu_max": 0}
+        if len(self.samples) < 2:
+            return {
+                "energy-j": 0.0,
+                "energy-kwh": 0.0,
+                "pow-avg": 0.0
+            }
+
+        energy_j = 0.0
+
+        for i in range(1, len(self.samples)): # Calculate energy used in joules
+            mw1, t1 = self.samples[i]
+            mw0, t0 = self.samples[i-1]
+            dt = t1 - t0
+            avg_power_w = (mw1 + mw0) / 2 / 1000 # Average power between two measurements, converted from mW to W, using trapezoidal rule
+            energy_j += avg_power_w * dt
+
+        total_time = self.samples[-1][1] - self.samples[0][1]
+        average_power_w = energy_j / total_time
+
+        energy_kwh = energy_j / 3600000
 
         return {
-            "nv-pow-mW": self.samples
+            "avg-util": sum(self.utils) / len(self.utils),
+            "peak-util": max(self.utils),
+            "energy-j": energy_j,
+            "energy-kwh": energy_kwh,
+            "pow-avg": average_power_w,
+            "raw": {
+                "pow": self.samples,
+                "utils": self.utils,
+            }
         }
+        
