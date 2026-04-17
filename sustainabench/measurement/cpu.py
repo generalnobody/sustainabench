201b11976ffc8517946a19b376205f55f11155b3
generalnobody <ingmar.biemond@gmail.com>
2026-04-17 15:06:25 +0200

    Wrote code to measure cpu_times_percent (currently commented since unsure if necessary)

--- sustainabench/measurement/cpu.py
+++ sustainabench/measurement/cpu.py
@@ -12,11 +12,15 @@ class CPUMeasurement(Measurement):
         self.samples = []
         psutil.cpu_percent(interval=None) # Ignore first bogus value
         psutil.cpu_percent(interval=None, percpu=True)
+        # psutil.cpu_times_percent(interval=None)
+        # psutil.cpu_times_percent(interval=None, percpu=True)
 
     def sample(self):
         self.samples.append((
             psutil.cpu_percent(interval=None),
-            psutil.cpu_percent(interval=None, percpu=True)
+            psutil.cpu_percent(interval=None, percpu=True),
+            # psutil.cpu_times_percent(interval=None)._asdict(),
+            # [t._asdict() for t in psutil.cpu_times_percent(interval=None, percpu=True)]
         ))
 
     def stop(self):
@@ -27,7 +31,7 @@ class CPUMeasurement(Measurement):
             return {"cpu_avg": 0, "cpu_max": 0, "per-core": {}}
 
         cpu_avg = sum(sample[0] for sample in self.samples) / len(self.samples)
-        cpu_max = sum(sample[0] for sample in self.samples)
+        cpu_max = max(sample[0] for sample in self.samples)
 
         per_cpu_avg = [
             sum(col) / len(col)
@@ -38,9 +42,39 @@ class CPUMeasurement(Measurement):
             for col in zip(*(sample[1] for sample in self.samples))
         ]
 
+        # cpu_times_avg = { # Should sum to approximately 100% (slight deviation possible due to floating point shift)
+        #     key: sum(sample[2][key] for sample in self.samples) / len(self.samples)
+        #     for key in self.samples[0][2]
+        # }
+
+        # cpu_times_max = { # Measures spikes. Is not expected to sum to 100%
+        #     key: max(sample[2][key] for sample in self.samples)
+        #     for key in self.samples[0][2]
+        # }
+
+        # per_cpu_times_avg = [] # Same logic as for cpu_times_avg
+        # for core_idx in range(len(self.samples[0][3])):
+        #     core_avg = {
+        #         key: sum(sample[3][core_idx][key] for sample in self.samples) / len(self.samples)
+        #         for key in self.samples[0][3][core_idx]
+        #     }
+        #     per_cpu_times_avg.append(core_avg)
+
+        # per_cpu_times_max = [] # Same logic as for cpu_times_max
+        # for core_idx in range(len(self.samples[0][3])):
+        #     core_max = {
+        #         key: max(sample[3][core_idx][key] for sample in self.samples)
+        #         for key in self.samples[0][3][core_idx]
+        #     }
+        #     per_cpu_times_max.append(core_max)
+
         return {
             "cpu_avg": cpu_avg,
             "cpu_max": cpu_max,
             "per_cpu_avg": per_cpu_avg,
-            "per_cpu_max": per_cpu_max
+            "per_cpu_max": per_cpu_max,
+            # "cpu_times_avg": cpu_times_avg,
+            # "cpu_times_max": cpu_times_max,
+            # "per_cpu_times_avg": per_cpu_times_avg,
+            # "per_cpu_times_max": per_cpu_times_max
         }
