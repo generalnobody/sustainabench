e8008a503b551f8261b2e7b3d50e64ebdce7f8dc
generalnobody <ingmar.biemond@gmail.com>
2026-04-20 11:32:17 +0200

    Added swap usage stats

--- sustainabench/measurement/memory.py
+++ sustainabench/measurement/memory.py
@@ -13,9 +13,12 @@ class MemoryMeasurement(Measurement):
 
     def sample(self):
         mem = psutil.virtual_memory()
-        used_mb = mem.used / 1024**2
-        percent = mem.percent
-        self.samples.append((used_mb, percent))
+        mem_used_mb = mem.used / 1024**2
+
+        swap = psutil.swap_memory()
+        swap_used_mb = swap.used / 1024**2
+
+        self.samples.append((mem_used_mb, mem.percent, swap_used_mb, swap.percent))
 
     def stop(self):
         pass
@@ -26,7 +29,24 @@ class MemoryMeasurement(Measurement):
 
         mem_avg = sum(sample[0] for sample in self.samples) / len(self.samples)
         mem_max = max(sample[0] for sample in self.samples)
+        mem_avg_pct = sum(sample[1] for sample in self.samples) / len(self.samples)
+        mem_max_pct = max(sample[1] for sample in self.samples)
+
+        swap_avg = sum(sample[2] for sample in self.samples) / len(self.samples)
+        swap_max = max(sample[2] for sample in self.samples)
+        swap_avg_pct = sum(sample[3] for sample in self.samples) / len(self.samples)
+        swap_max_pct = max(sample[3] for sample in self.samples)
         return {
-            "mem_avg_mb": mem_avg,
-            "mem_max_mb": mem_max
+            "mem": {
+                "avg_mb": mem_avg,
+                "max_mb": mem_max,
+                "avg_pct": mem_avg_pct,
+                "max_pct": mem_max_pct
+            },
+            "swap": {
+                "avg_mb": swap_avg,
+                "max_mb": swap_max,
+                "avg_pct": swap_avg_pct,
+                "max_pct": swap_max_pct
+            }
         }
