0cb34aa52abd83cb795159ddfeafbb7cfc61c2b1
generalnobody <ingmar.biemond@gmail.com>
2026-04-20 12:29:46 +0200

    Added CPU freq tracking. TODO: add CPU temp tracking

--- sustainabench/measurement/cpu.py
+++ sustainabench/measurement/cpu.py
@@ -4,7 +4,7 @@ from sustainabench.measurement.base import Measurement, register_measurement
 
 @register_measurement
 class CPUMeasurement(Measurement):
-    name = "cpu-usage"
+    name = "cpu"
     poll_interval = 0.1
     scope = "node"
 
@@ -15,10 +15,23 @@ class CPUMeasurement(Measurement):
         # psutil.cpu_times_percent(interval=None)
         # psutil.cpu_times_percent(interval=None, percpu=True)
 
+        ################# Test #################
+        temps = psutil.sensors_temperatures()
+        for name, entries in temps.items():
+            print(f"{name}:")
+            for entry in entries:
+                print(f"  {entry.label or 'N/A'}: {entry.current}°C")
+        ########################################
+
     def sample(self):
+        cpu_freq = psutil.cpu_freq()
+        per_cpu_freq = psutil.cpu_freq(percpu=True)
+
         self.samples.append((
             psutil.cpu_percent(interval=None),
             psutil.cpu_percent(interval=None, percpu=True),
+            cpu_freq.current,
+            [freq.current for freq in per_cpu_freq]
             # psutil.cpu_times_percent(interval=None)._asdict(),
             # [t._asdict() for t in psutil.cpu_times_percent(interval=None, percpu=True)]
         ))
@@ -30,6 +43,7 @@ class CPUMeasurement(Measurement):
         if not self.samples:
             return {}
 
+        # Usage
         cpu_avg = sum(sample[0] for sample in self.samples) / len(self.samples)
         cpu_max = max(sample[0] for sample in self.samples)
 
@@ -42,39 +56,91 @@ class CPUMeasurement(Measurement):
             for col in zip(*(sample[1] for sample in self.samples))
         ]
 
+
+        # Frequency
+        cpu_freq = psutil.cpu_freq()
+        per_cpu_freq = psutil.cpu_freq(percpu=True)
+
+        cpu_freq_avg = sum(sample[2] for sample in self.samples) / len(self.samples)
+        percore_freq_avg = [
+            sum(values) / len(values)
+            for values in zip(*(sample[3] for sample in self.samples))
+        ]
+
+        return {
+            "cpu": {
+                "usage": {
+                    "avg": cpu_avg,
+                    "max": cpu_max,
+                },
+                "frequency": {
+                    "min": cpu_freq.min,
+                    "max": cpu_freq.max,
+                    "avg": cpu_freq_avg,
+                },
+            },
+            "percpu": [
+                {
+                    "core": i,
+                    "usage": {
+                        "avg": u_avg,
+                        "max": u_max,
+                    },
+                    "frequency": {
+                        "avg": f_avg,
+                        "min": freq.min,
+                        "max": freq.max,
+                    },
+                }
+                for i, (u_avg, u_max, f_avg, freq) in enumerate(
+                    zip(per_cpu_avg, per_cpu_max, percore_freq_avg, per_cpu_freq)
+                )
+            ]
+        }
+
+        # percpu = [
+        #     {
+        #         "core": i,
+        #         "avg": avg,
+        #         "min": freq.min,
+        #         "max": freq.max,
+        #     }
+        #     for i, (avg, freq) in enumerate(zip(percore_freq_avg, per_cpu_freq))
+        # ]
+
         # cpu_times_avg = { # Should sum to approximately 100% (slight deviation possible due to floating point shift)
-        #     key: sum(sample[2][key] for sample in self.samples) / len(self.samples)
-        #     for key in self.samples[0][2]
+        #     key: sum(sample[4][key] for sample in self.samples) / len(self.samples)
+        #     for key in self.samples[0][4]
         # }
 
         # cpu_times_max = { # Measures spikes. Is not expected to sum to 100%
-        #     key: max(sample[2][key] for sample in self.samples)
-        #     for key in self.samples[0][2]
+        #     key: max(sample[4][key] for sample in self.samples)
+        #     for key in self.samples[0][4]
         # }
 
         # per_cpu_times_avg = [] # Same logic as for cpu_times_avg
-        # for core_idx in range(len(self.samples[0][3])):
+        # for core_idx in range(len(self.samples[0][5])):
         #     core_avg = {
-        #         key: sum(sample[3][core_idx][key] for sample in self.samples) / len(self.samples)
-        #         for key in self.samples[0][3][core_idx]
+        #         key: sum(sample[5][core_idx][key] for sample in self.samples) / len(self.samples)
+        #         for key in self.samples[0][5][core_idx]
         #     }
         #     per_cpu_times_avg.append(core_avg)
 
         # per_cpu_times_max = [] # Same logic as for cpu_times_max
-        # for core_idx in range(len(self.samples[0][3])):
+        # for core_idx in range(len(self.samples[0][5])):
         #     core_max = {
-        #         key: max(sample[3][core_idx][key] for sample in self.samples)
-        #         for key in self.samples[0][3][core_idx]
+        #         key: max(sample[5][core_idx][key] for sample in self.samples)
+        #         for key in self.samples[0][5][core_idx]
         #     }
         #     per_cpu_times_max.append(core_max)
 
-        return {
-            "cpu_avg": cpu_avg,
-            "cpu_max": cpu_max,
-            "per_cpu_avg": per_cpu_avg,
-            "per_cpu_max": per_cpu_max,
-            # "cpu_times_avg": cpu_times_avg,
-            # "cpu_times_max": cpu_times_max,
-            # "per_cpu_times_avg": per_cpu_times_avg,
-            # "per_cpu_times_max": per_cpu_times_max
-        }
+        # return {
+        #     "cpu_avg": cpu_avg,
+        #     "cpu_max": cpu_max,
+        #     "per_cpu_avg": per_cpu_avg,
+        #     "per_cpu_max": per_cpu_max,
+        #     # "cpu_times_avg": cpu_times_avg,
+        #     # "cpu_times_max": cpu_times_max,
+        #     # "per_cpu_times_avg": per_cpu_times_avg,
+        #     # "per_cpu_times_max": per_cpu_times_max
+        # }
