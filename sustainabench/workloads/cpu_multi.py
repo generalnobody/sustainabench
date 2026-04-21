0122e3de87fc711781bf3d0f24c79710f39112cb
generalnobody <ingmar.biemond@gmail.com>
2026-04-21 15:06:17 +0200

    Small fix

--- sustainabench/workloads/cpu_multi.py
+++ sustainabench/workloads/cpu_multi.py
@@ -1,26 +1,3 @@
-# import multiprocessing as mp
-# import time
-
-# def work(n):
-#     count = 0
-#     for i in range(2, n):
-#         prime = True
-#         for j in range(2, int(i**0.5)+1):
-#             if i % j == 0:
-#                 prime = False
-#                 break
-#         if prime:
-#             count += 1
-#     return count
-
-# def run(workers=mp.cpu_count(), n=20000):
-#     start = time.time()
-
-#     with mp.Pool(workers) as pool:
-#         results = pool.map(work, [n]*workers)
-
-#     print(f"[cpu_multi] total primes: {sum(results)} in {time.time()-start:.2f}s")
-
 import multiprocessing as mp
 from sustainabench.workloads.base import Workload, register_workload
 
