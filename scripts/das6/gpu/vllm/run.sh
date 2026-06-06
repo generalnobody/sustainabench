92f727f78f99774fdb9a9a6224be5bb663274ae7
generalnobody <ingmar.biemond@gmail.com>
2026-06-07 00:07:08 +0200

    Added VLLM benchmark script for DAS6

---
+++ scripts/das6/gpu/vllm/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=sustainabench_gpu_vllm
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus=1
+#SBATCH --constraint=A4000
+#SBATCH --time=01:00:00
+
+RUNS=5
+
+echo "Running VLLM experiments"
+sustainabench run benchmark -w vllm -m time -m likwid=configs/likwid.yaml -m gpu-nvidia -r $RUNS -c configs/vllm.yaml -s
