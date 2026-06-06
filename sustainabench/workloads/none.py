283c17b328071a6f4ed69da70a167aa4945bc5be
generalnobody <ingmar.biemond@gmail.com>
2026-06-06 20:24:51 +0200

    Added dummy workload

---
+++ sustainabench/workloads/none.py
@@ -0,0 +1,9 @@
+from sustainabench.workloads.base import InternalWorkload, register_workload
+
+@register_workload
+class NoneWorkload(InternalWorkload):
+    """Dummy workload only used for testing"""
+    name = "cpu-single"
+
+    def run(self, num_processors: int, context=None):
+        pass
