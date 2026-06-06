eafcaa06cc8aa6a33c6a45b8d94c8cadae94dacd
generalnobody <ingmar.biemond@gmail.com>
2026-06-06 20:56:39 +0200

    Rename file and made it so that pypmt absence won't block program execution

--- sustainabench/measurement/__init__.py
+++ sustainabench/measurement/__init__.py
@@ -3,6 +3,10 @@ from pathlib import Path
 
 from .base import MEASUREMENTS, register_measurement
 
+skip_if_missing = {
+    "rapl_pypmt": "pypmt",
+}
+
 package_dir = Path(__file__).parent
 
 # Import of all relevant measurements
@@ -10,6 +14,13 @@ for file in package_dir.rglob("*.py"):
     if file.stem in ("__init__", "base", "manager"):
         continue    
         
+    if file.stem in skip_if_missing:
+        dep = skip_if_missing[file.stem]
+        try:
+            import_module(dep)
+        except ModuleNotFoundError:
+            continue  # skip this file entirely
+
     rel_path = file.relative_to(package_dir)
 
     module_parts = rel_path.with_suffix("").parts
