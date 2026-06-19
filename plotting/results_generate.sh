8611f386d3f6a7b4e42daece684664b85c4bbec6
generalnobody <ingmar.biemond@gmail.com>
2026-06-19 14:08:51 +0200

    Generated additional results for Snellius PL and Snellius 2023

---
+++ plotting/results_generate.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+TARGET_DIRS=(
+    "scripts/snellius/cpu/rome/"
+    "scripts/snellius/cpu/genoa/"
+    "scripts/snellius/gpu/a100/"
+    "scripts/snellius/gpu/h100/"
+)
+
+FILE_SOURCE_DIR="experiments/merged/"
+
+for target_dir in "${TARGET_DIRS[@]}"; do
+    echo "Generating standard results in: $target_dir"
+    (
+        cd "$target_dir"
+
+        for file in "$FILE_SOURCE_DIR"/*; do
+            [[ -f "$file" ]] || continue
+
+            # Replace with your actual command
+            sustainabench result generate -m carbon=../../../../traces/carbon/traces/NL_2021-2024.parquet -m energy-to-solution -m performance-per-carbon -m carbon-per-second -m all-carbon -m max-execution-time -md ../../../../configs/metrics/metrics_dict.yaml -c ../../../../configs/metrics/main.yaml -s -r "$file"
+        done
+    )
+done
+
+for target_dir in "${TARGET_DIRS[@]}"; do
+    echo "Generating PL results in: $target_dir"
+    (
+        cd "$target_dir"
+
+        for file in "$FILE_SOURCE_DIR"/*; do
+            [[ -f "$file" ]] || continue
+
+            # Replace with your actual command
+            sustainabench result generate -m carbon=../../../../traces/carbon/traces/PL_2021-2024.parquet -m energy-to-solution -m performance-per-carbon -m carbon-per-second -m all-carbon -m max-execution-time -md ../../../../configs/metrics/metrics_dict.yaml -c ../../../../configs/metrics/main.yaml -o ./experiments/results/PL/ -s -r "$file"
+        done
+    )
+done
+
+for target_dir in "${TARGET_DIRS[@]}"; do
+    echo "Generating early results in: $target_dir"
+    (
+        cd "$target_dir"
+
+        for file in "$FILE_SOURCE_DIR"/*; do
+            [[ -f "$file" ]] || continue
+
+            # Replace with your actual command
+            sustainabench result generate -m carbon=../../../../traces/carbon/traces/NL_2021-2024.parquet -m energy-to-solution -m performance-per-carbon -m carbon-per-second -m all-carbon -m max-execution-time -md ../../../../configs/metrics/metrics_dict.yaml -c ../../../../configs/metrics/main.yaml -o ./experiments/results/2023/ -s -r "$file"
+        done
+    )
+done
